980c72b26461d4ab9f6b71fcb02166ea4505e252
Logan Barsell <loganbarsell@MacBook-Pro.local>
2022-08-19 03:45:50 -0700

    HOME PAGE

---
+++ src/components/Footer.jsx
@@ -0,0 +1,127 @@
+import { Facebook, Instagram, Mail, Phone, Pinterest, Room, Twitter } from '@material-ui/icons'
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    display: flex;
+`
+const Left = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+`
+
+const Logo = styled.h1``;
+const Desc = styled.p`
+    margin: 20px 0px;
+`;
+
+const SocialContainer = styled.div`
+    display: flex;
+
+`;
+const SocialIcon = styled.div`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    color: white;
+    background-color: #${props=>props.color};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: 20px;
+`;
+
+const Center = styled.div`
+    flex: 1;
+    padding: 20px;
+`
+
+const Title = styled.h3`
+    margin-bottom: 30px;
+`
+
+const List = styled.ul`
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    display: flex;
+    flex-wrap: wrap;
+`
+
+const ListItem = styled.li`
+    width: 50%;
+    margin-bottom: 10px;
+`
+
+const Right = styled.div`
+    flex: 1;
+    padding: 20px;
+`
+
+const ContactItem = styled.div`
+    margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+`
+
+const Payment = styled.img`
+    width: 50%;
+`
+
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>myStore</Logo>
+        <Desc>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eaque, quam modi sint harum atque minus doloremque consectetur quisquam officia rerum necessitatibus eligendi? Eius nam ipsa molestiae, quos harum saepe voluptates!
+        </Desc>
+        <SocialContainer>
+            <SocialIcon color="3B5999">
+                <Facebook/>
+            </SocialIcon>
+            <SocialIcon color="E4405F">
+                <Instagram/>
+            </SocialIcon>
+            <SocialIcon color="55ACEE">
+                <Twitter/>
+            </SocialIcon>
+            <SocialIcon color="E60023">
+                <Pinterest/>
+            </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Links</Title>
+        <List>
+            <ListItem>Home</ListItem>
+            <ListItem>Cart</ListItem>
+            <ListItem>Apparel</ListItem>
+            <ListItem>Accessories</ListItem>
+            <ListItem>My Account</ListItem>
+            <ListItem>Orders</ListItem>
+            <ListItem>Wislist</ListItem>
+            <ListItem>About Us</ListItem>
+            <ListItem>Careers</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+            <Room style={{ marginRight: "10px"}}/> East San Francisco Bay Area, CA
+        </ContactItem>
+        <ContactItem>
+            <Phone style={{ marginRight: "10px"}}/> +1 (925)262-7761
+        </ContactItem>
+        <ContactItem>
+            <Mail style={{ marginRight: "10px"}}/> contact@loganbarsell.com
+        </ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png"/>
+      </Right>
+    </Container>
+  )
+}
+
+export default Footer
